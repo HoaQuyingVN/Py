bb5edd606f55b72dd8fbb97246125ac742e587da
HoaQuyingVN <126839568+HoaQuyingVN@users.noreply.github.com>
2023-03-03 15:11:27 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(
-    name='dnd-tools',
+    name='pinyin',
     version='0.1.5',
     license='AGPL',
     scripts=[
-        'scripts/dnd-tools'
+        'scripts/pinyin'
     ]
 )
